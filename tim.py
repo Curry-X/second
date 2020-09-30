c0bd42d569db0eda047edbafed18cea6d2c4fa47
CurryX <1071375521@qq.com>
2020-09-30 10:54:19 +0800

    new file

---
+++ tim.py
@@ -0,0 +1,26 @@
+import functools
+
+def log(func=''):
+        def decorators(func): 
+            @functools.wraps(func)
+            def w(*args,**kw):
+                print('Curry_Xu 来也！！！%s'%(func.__name__))
+                func(*args,**kw)
+                print("哦吼，拜拜~~~")
+            return w 
+        return decorators  
+      
+    
+@log()
+def f():
+    print("我是") #log()(f)
+
+@log('nishi')
+def nwo():
+    x=12
+    y=6
+    z=y+x
+    print(z)# log('nishi')(nwo)
+
+f()
+nwo()
